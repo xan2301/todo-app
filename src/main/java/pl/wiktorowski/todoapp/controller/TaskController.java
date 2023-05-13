e7d01cd08f2880edb7101ec6b6ba5f7378d938c8
xan2301 <2silverghost2@gmail.com>
2023-05-13 17:02:09 +0200

    add @GetMapping

--- src/main/java/pl/wiktorowski/todoapp/controller/TaskController.java
+++ src/main/java/pl/wiktorowski/todoapp/controller/TaskController.java
@@ -5,7 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.wiktorowski.todoapp.model.Task;
 import pl.wiktorowski.todoapp.model.TaskRepository;
+
+import java.util.List;
 
 @RepositoryRestController
 
@@ -13,12 +20,22 @@ public class TaskController {
 
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
-    private  final TaskRepository repository;
-
+    private final TaskRepository repository;
 
 
     @Autowired
     public TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
+
+    @GetMapping("/tasks")
+
+    ResponseEntity<?> readAllTasks(){
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+
+
+
 }
