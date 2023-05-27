ab8175daa50bb309dff524f9ac1d73da4ae73f59
xan2301 <2silverghost2@gmail.com>
2023-05-27 15:47:55 +0200

    add  Pageable

--- src/main/java/pl/wiktorowski/todoapp/controller/TaskController.java
+++ src/main/java/pl/wiktorowski/todoapp/controller/TaskController.java
@@ -4,6 +4,7 @@ package pl.wiktorowski.todoapp.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,22 +13,24 @@ import pl.wiktorowski.todoapp.model.TaskRepository;
 @RepositoryRestController
 
 public class TaskController {
-
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
-
     private final TaskRepository repository;
-
 
     @Autowired
     public TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/tasks")
-
+    @GetMapping(value = "/tasks", params= {"!sort","!page","!size"})
     ResponseEntity<?> readAllTasks(){
         logger.warn("Exposing all the tasks!!!");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping( "/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page){
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page));
     }
 
 
